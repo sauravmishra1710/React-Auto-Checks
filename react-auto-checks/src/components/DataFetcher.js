60a77f99e411ee31c6632865ac2a23c590c97e3e
Saurav Mishra <saurav.mishra@live.com>
2025-01-09 20:44:19 +0530

    fetch users & jest tests

---
+++ react-auto-checks/src/components/DataFetcher.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DataFetcher() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        setUsers(response.data);
+      } catch (err) {
+        setError('Failed to fetch data');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // eslint-disable-next-line react/jsx-filename-extension
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DataFetcher;
