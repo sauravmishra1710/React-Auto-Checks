6766f577637573539fa26d3a84460d7cadee1e65
Saurav Mishra <saurav.mishra@live.com>
2025-01-10 23:38:38 +0530

    husky configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 export default {
   presets: [
+    [
     '@babel/preset-env', // Transpile ES6+
     '@babel/preset-react', // Transpile JSX
-  ],
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
 };
